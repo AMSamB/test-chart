bef44d87f743116352aac32b1cfa201f56c140f4
AMSamB <97667129+AMSamB@users.noreply.github.com>
2022-01-13 11:14:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
   ];
 
   // x-axis label and label in tooltip
-  var X_AXIS = 'Test';
+  var X_AXIS = '';
 
   // y-axis label, label in tooltip
   var Y_AXIS = 'Share';
