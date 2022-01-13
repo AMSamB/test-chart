f482cea3ad99f10402ead4725c5259906befd6a3
AMSamB <97667129+AMSamB@users.noreply.github.com>
2022-01-13 11:33:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ $(document).ready(function() {
             ticks: {
               beginAtZero: true,
               callback: function(value, index, values) {
-                return value.toLocaleString()
+                return value.toLocaleString('de-DE', {style:'percent'})
               }
             }
           }]
